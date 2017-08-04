c25cd8c3d8f681f70c3c88238d94980232afa712
Maojiong.Fu <792657655@qq.com>
2017-08-04 11:43:56 +0800

    添加程序集信息

--- LookMyNet/Properties/AssemblyInfo.cs
+++ LookMyNet/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("LookMyNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("网速监控小工具条")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/qljiong/LookMyNet")]
 [assembly: AssemblyProduct("LookMyNet")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
